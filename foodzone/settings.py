faf53f5c7a7af3259cceccc35f7dc079aa830d2c
parasjee2002 <parasjee2000@icloud.com>
2024-01-25 19:08:37 +0530

    changes

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-+06=x1aer^924_h5a5+l0sr)fb8p-9xd+#ctxqib1bi^ztu5dw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
