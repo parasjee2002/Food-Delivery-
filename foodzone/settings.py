84db4ab86da13f7f66ab3dae85de7c06b9820895
parasjee2002 <parasjee2000@icloud.com>
2024-01-28 16:17:07 +0530

    changes

--- foodzone/settings.py
+++ foodzone/settings.py
@@ -62,15 +62,10 @@ WSGI_APPLICATION = 'foodzone.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
